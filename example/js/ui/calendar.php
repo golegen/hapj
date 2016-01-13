399f63c69bc2af3b32505f242d1fe8b09b7a9bee
zhangyuemei <zhangyuemei@jiehun.com.cn>
2016-01-13 21:57:14 +0800

    calendar.js fixed

--- example/js/ui/calendar.php
+++ example/js/ui/calendar.php
@@ -13,20 +13,33 @@ include '../../include/header.phtml';
     <li>
         <h2>绑定到input控件的日历</h2>
         <div id="calendar"></div>
-
-        <label>日期</label><input type="text" name="start_date" id="txtStartDate"/>
+        <label>日期：</label><input type="text" name="start_date" id="txtStartDate"/>
+        <br>
         <script>
             var cal = $('#calendar').calendar({
                 input: $('#txtStartDate'),
                 format:'Y/m/d'
             });
         </script>
+        <label class="label label-primary">Html:</label>
+        <pre><code class="html">
+         &lt;div id="calendar"&gt;&lt;/div&gt;
+         &lt;label>日期：&lt;/label&gt; &lt;input type="text" name="start_date" id="txtStartDate"/&gt;
+         </code></pre>
+
+        <label class="label label-primary">Javascript:</label>
+        <pre><code class="javascript">
+         var cal = $('#calendar').calendar({
+            input: $('#txtStartDate'),
+            format:'Y/m/d'
+         });
+        </code></pre>
     </li>
     <li>
         <h2>绑定到input控件的日历（有默认时间）</h2>
         <div id="calendar6"></div>
 
-        <label>日期</label><input type="text" name="start_date" id="txtStartDate2" value="2014/08/25 23:22"/>
+        <label>日期：</label><input type="text" name="start_date" id="txtStartDate2" value="2014/08/25 23:22"/>
         <script>
             var cal = $('#calendar6').calendar({
                 input: $('#txtStartDate2'),
@@ -34,6 +47,21 @@ include '../../include/header.phtml';
                 format:'Y/m/d H:i'
             });
         </script>
+        <br>
+        <label class="label label-primary">Html:</label>
+        <pre><code class="html">
+            &lt;div id="calendar6"&gt;&lt;/div&gt;
+            &lt;label>日期：&lt;/label&gt;&lt;input type="text" name="start_date" id="txtStartDate2" value="2014/08/25 23:22"/&gt;
+        </code></pre>
+
+        <label class="label label-primary">Javascript:</label>
+        <pre><code class="javascript">
+        var cal = $('#calendar6').calendar({
+            input: $('#txtStartDate2'),
+            timeType:2,
+            format:'Y/m/d H:i'
+        });
+        </code></pre>
     </li>
 
     <li>
@@ -47,6 +75,20 @@ include '../../include/header.phtml';
                 }
             });
         </script>
+        <br>
+        <label class="label label-primary">Html:</label>
+        <pre><code class="html">
+            &lt;div id="calendar3"&gt; &lt;/div&gt;
+        </code></pre>
+
+        <label class="label label-primary">Javascript:</label>
+        <pre><code class="javascript">
+         $('#calendar3').calendar({
+            onSelect:function(y, m, d) {
+            alert(y + '-' + m + '-' + d);
+            }
+         });
+        </code></pre>
     </li>
     <li>
         <h2>直接显示的日历(含时间)</h2>
@@ -62,6 +104,23 @@ include '../../include/header.phtml';
                 }
             });
         </script>
+        <br>
+        <label class="label label-primary">Html:</label>
+        <pre><code class="html">
+            &lt;div id="calendar2"&gt; &lt;/div&gt;
+        </code></pre>
+
+        <label class="label label-primary">Javascript:</label>
+        <pre><code class="javascript">
+        $('#calendar2').calendar({
+            startDate:'2014-09-25',
+            endDate:'2014-09-30',
+            timeType:3,
+            onSelect:function(y, m, d, h, i, s) {
+            alert(y + '-' + m + '-' + d + ' ' + h + ':' + i + ':' + s);
+            }
+        });
+        </code></pre>
     </li>
 </ol>
 <?php include '../../include/footer.phtml' ?>
