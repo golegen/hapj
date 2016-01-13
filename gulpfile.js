9a623b2a4e4d8e042998160da9f5cf07de8d88ef
dengxiaolong <dengxiaolong@jiehun.com.cn>
2016-01-13 11:09:40 +0800

    改进gulp任务帮助系统

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 var jsSrc = [
-    './src/js/*.js',
-    './src/js/ui/*.js'
+    './src/js/**/*.js'
+];
+var helps = [
+    ['js', 'js生成、压缩'],
+    ['css', 'css生成、压缩'],
+    ['lint', 'js代码检查'],
+    ['watch', '监控文件变化，并自动生成和压缩文件'],
+    ['mardown', '生成markdown对应的html文件'],
+    ['doc', '生成doc文档'],
 ];
 
 gulp.task('default', function () {
-    console.log('hello,hapj!');
+    var codes = [];
+    codes.push("帮助：");
+    helps.forEach(function(v, k) {
+        codes.push('  gulp ' + v[0]);
+        codes.push('    ' + v[1]);
+        codes.push("\n");
+    });
+    console.log(codes.join("\n"));
 });
 
 // js代码编译
@@ -18,8 +33,8 @@ gulp.task('lint', function () {
 });
 
 // 生成markdown
-gulp.task('markdown', function(fb) {
-    var md = require( 'markdown' ).markdown;
+gulp.task('markdown', function (fb) {
+    var md = require('markdown').markdown;
     var fs = require('fs');
     var content = fs.readFileSync('./readme.md', 'utf-8');
     var html = md.toHTML(content);
@@ -41,12 +56,11 @@ gulp.task('doc', function () {
 
 // 压缩代码
 gulp.task('min', function () {
-    var uglify = require('gulp-uglify');
     var rename = require('gulp-rename');
     var del = require('del');
     var cssmin = require('gulp-cssmin');
 
-    del.sync('./dist/');
+    del.sync('./dist/js/');
 
     // jquery
     gulp.src('./bower_components/jquery/dist/jquery.min.js')
@@ -80,16 +94,15 @@ gulp.task('min', function () {
     gulp.src('./bower_components/highlightjs/styles/*.png')
         .pipe(gulp.dest('./dist/js/highlightjs/css/'));
 
-    return gulp.src('./src/js/*.js')
+    return gulp.src(jsSrc)
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('./dist/js/hapj/'));
-
 });
 
-gulp.task('concat', ['min'], function () {
+gulp.task('js', ['min'], function () {
     var concat = require('gulp-concat');
     return gulp.src([
             './dist/js/jquery/jquery.min.js',
@@ -99,5 +112,46 @@ gulp.task('concat', ['min'], function () {
         ])
         .pipe(concat('hapj.example.js', {newLine: ';'}))
         .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('watch', function () {
+    var path = require('path');
+    gulp.watch([
+        './src/js/**/*.js',
+        './src/css/**/*.css',
+        './src/css/**/*.less'
+    ], function(evt) {
+        console.log(evt.type + ' ' + evt.path);
+        var ext = path.extname(evt.path);
+        switch(ext) {
+            case '.js':
+                gulp.run(['js']);
+                break;
+            case '.css':
+            case '.less':
+                gulp.run(['css']);
+                break;
+        }
+    });
+
+});
+
+// less任务
+gulp.task('css', function () {
+    var del = require('del');
+    var less = require('gulp-less');
+    var rename = require('gulp-rename');
+    del.sync('/dist/css');
+
+    return gulp.src(
+        './src/css/**/*.less'
+        )
+        .pipe(less())
+        .pipe(uglify())
+        .pipe(rename({
+            suffix:'.min'
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+    ;
 });
 
